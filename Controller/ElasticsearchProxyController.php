bcc4df0570afa3ec0417b1d18f73d9cd5c118a4e
Venkat Krishna Kotra <kotrakrishna@gmail.com>
2014-01-28 20:33:40 +0530

    - Check if it is request body search. If no payload sent, throw bad request exception. Check if variable is an array before running it through foreach. Fix comparison operator issue. User identical operator. Fix other minor issues.

--- Controller/ElasticsearchProxyController.php
+++ Controller/ElasticsearchProxyController.php
@@ -2,8 +2,8 @@
 namespace Xola\ElasticsearchProxyBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
 use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -15,14 +15,17 @@ class ElasticsearchProxyController extends Controller
     {
         $user = $this->get('security.context')->getToken()->getUser();
 
-        if (!$user || !$user instanceof UserInterface) {
+        if(!$user || !$user instanceof UserInterface) {
             // User not authenticated
             throw new UnauthorizedHttpException('');
         }
 
-
         // Get content for passing on in elastic search request
         $data = json_decode($request->getContent(), true);
+
+        if(!$data) {
+            throw new BadRequestHttpException();
+        }
 
         // Authorisation filter is the filter on seller
         $authFilter = array('term' => array('seller.id' => $user->getId()));
@@ -31,9 +34,7 @@ class ElasticsearchProxyController extends Controller
         // Inject authorisation filter
         $this->addAuthFilter($data, $authFilter, $filterCounter);
 
-
-
-        if ($filterCounter['applied'] <= 0 || $filterCounter['notApplied'] > 0) {
+        if($filterCounter['applied'] <= 0 || $filterCounter['notApplied'] > 0) {
             // Authorisation filter could not be applied. Bad Request.
             throw new BadRequestHttpException();
         }
@@ -46,7 +47,7 @@ class ElasticsearchProxyController extends Controller
         $config = $this->container->getParameter('xola_elasticsearch_proxy');
         $url = $config['client']['host'] . ':' . $config['client']['port'] . '/' . $slug;
 
-        if ($query) {
+        if($query) {
             // Query string exists. Add it to the url
             $url .= '?' . $query;
         }
@@ -65,10 +66,10 @@ class ElasticsearchProxyController extends Controller
         $curlInfo = curl_getinfo($ch);
         curl_close($ch);
 
-        if ($response === false) {
-            return new JsonResponse('', 404);
+        if($response === false) {
+            return new Response('', 404);
         } else {
-            return new JsonResponse($response, $curlInfo['http_code']);
+            return new Response($response, $curlInfo['http_code'], array('Content-Type' => 'application/json'));
         }
     }
 
@@ -84,30 +85,32 @@ class ElasticsearchProxyController extends Controller
      * @param array $filterCounter  No. of times the filter was applied/not. Has keys 'applied' and 'notApplied' in it.
      *                              For an applicable filter, if authorisation is added, 'applied' gets incremented.
      *                              Else 'notApplied' gets incremented.
-     * @param int $appliedFilter    Applicable only when called recursively. Incremented each time the authFilter gets applied.
-     * @param bool $isQuery         Flag to indicate if $query is an elastic search query field or a child array of it.
+     * @param int   $appliedFilter  Applicable only when called recursively. Incremented each time the authFilter gets applied.
+     * @param bool  $isQuery        Flag to indicate if $query is an elastic search query field or a child array of it.
      */
     private function addAuthFilter(&$query, $authFilter, &$filterCounter, &$appliedFilter = 0, $isQuery = false)
     {
+        if(!is_array($query)) return;
+
         // Set default values if null specified.
-        if (!is_array($filterCounter)) {
+        if(!is_array($filterCounter)) {
             $filterCounter = array();
         }
         // Filter counter must have key 'applied'. No. of filters where authorisation was added. This should
         // ideally be positive to ensure that the query was added with authorisation filter at least once.
-        if (!isset($filterCounter['applied'])) $filterCounter['applied'] = 0;
+        if(!isset($filterCounter['applied'])) $filterCounter['applied'] = 0;
 
         // Filter counter must have key 'notApplied'. No. of filters where addition of authorisation was missed. This
         // should ideally be zero, i.e we have added authorisation on all filters where ever applicable
-        if (!isset($filterCounter['notApplied'])) $filterCounter['notApplied'] = 0;
+        if(!isset($filterCounter['notApplied'])) $filterCounter['notApplied'] = 0;
 
-        foreach ($query as $key => $val) {
+        foreach($query as $key => $val) {
 
-            if ($key === 'filter') {
-                if ($isQuery) {
+            if($key === 'filter') {
+                if($isQuery) {
                     // This is filter within a query. Fine
-                    if (!empty($query[$key]['bool'])) {
-                        if (!is_array($query[$key]['bool']['must'])) {
+                    if(!empty($query[$key]['bool'])) {
+                        if(!is_array($query[$key]['bool']['must'])) {
                             $query[$key]['bool']['must'] = array();
                         }
                         array_push($query[$key]['bool']['must'], $authFilter);
@@ -119,7 +122,7 @@ class ElasticsearchProxyController extends Controller
                 // $filterCounter['applied']
             } else {
 
-                if ($key == 'query' && !$isQuery) {
+                if($key === 'query' && !$isQuery) {
                     // This is a top level query field.
 
                     // Counter to check how many times filter was applied within this array
@@ -128,7 +131,7 @@ class ElasticsearchProxyController extends Controller
                     $this->addAuthFilter($query[$key], $authFilter, $filterCounter, $applyFilterCount, true);
 
                     // This was a top level query field. Check if the Auth filter was added within this.
-                    if ($applyFilterCount <= 0) {
+                    if($applyFilterCount <= 0) {
                         // Filter was not applied to this query. Not right. This is a top level query and an
                         // authorisation filter was supposed to get added within it.
                         $filterCounter['notApplied'] += 1;
@@ -137,7 +140,7 @@ class ElasticsearchProxyController extends Controller
                         $filterCounter['applied'] += 1;
                     }
                 } else {
-                    $this->addAuthFilter($query[$key], $authFilter, $filterApplied, $isQuery);
+                    $this->addAuthFilter($query[$key], $authFilter, $filterCounter, $appliedFilter, $isQuery);
                 }
             }
 
