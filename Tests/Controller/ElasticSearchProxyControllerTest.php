e1bf915e235cb255adb5a4359352c30950a84d28
Anush Ramani <anush@xola.com>
2019-12-28 17:17:35 -0800

    XOL-4975 Upgrade to Symfony 3

--- Tests/Controller/ElasticSearchProxyControllerTest.php
+++ Tests/Controller/ElasticSearchProxyControllerTest.php
@@ -2,19 +2,20 @@
 
 namespace Xola\ElasticsearchProxyBundle\Tests\Controller;
 
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 use Xola\ElasticsearchProxyBundle\Controller\ElasticsearchProxyController;
-use Xola\ElasticsearchProxyBundle\Tests\Mocks\MockUser;
 
-class ElasticsearchProxyControllerTest extends \PHPUnit_Framework_TestCase
+class ElasticsearchProxyControllerTest extends TestCase
 {
-    /**
-     * @param array $params
-     *
-     * @return ElasticsearchProxyController
-     */
-    public function buildController($params = array())
+    /** @var ElasticsearchProxyController */
+    private $controller;
+
+    public function setUp()
     {
-        $container = $this->getMock("Symfony\Component\DependencyInjection\ContainerInterface");
+        parent::setUp();
+
+        $container = $this->createMock(ContainerInterface::class);
         $config = array(
             'client' => array(
                 'indexes' => array('logs'),
@@ -24,29 +25,17 @@ class ElasticsearchProxyControllerTest extends \PHPUnit_Framework_TestCase
             ),
             'roles_skip_auth_filter' => array()
         );
-        $container->expects($this->any())
-            ->method("getParameter")
-            ->with("xola_elasticsearch_proxy")
-            ->will($this->returnValue($config));
-        $defaults['container'] = $container;
+        $container->expects($this->any())->method("getParameter")->with("xola_elasticsearch_proxy")
+            ->willReturn($config);
 
-        $params = array_merge($defaults, $params);
-
-        // Get controller with mocked getUser method
-        $controllerBuilder = $this->getMockBuilder(
-            'Xola\ElasticsearchProxyBundle\Controller\ElasticsearchProxyController'
-        );
-        $controller = $controllerBuilder->setMethods(array('getUser'))->getMock();
-
-        $controller->setContainer($params['container']);
-
-        return $controller;
+        $this->controller = new ElasticsearchProxyController();
+        $this->controller->setContainer($container);
     }
+
 
     public function testGetElasticSearchUrl()
     {
-        $controller = $this->buildController();
-        $url = $controller->getElasticSearchUrl('pretty=true', 'logs', '_search');
+        $url = $this->controller->getElasticSearchUrl('pretty=true', 'logs', '_search');
 
         $this->assertEquals('http://localhost:9200/logs/_search?pretty=true', $url, 'Should be url built from proxy');
     }
